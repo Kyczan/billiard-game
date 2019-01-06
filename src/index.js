ca015fe63ad014d24ea41946c0efa38cdb0f1c0a
Krystian Kowal <krystian@kowal.pro>
2019-01-06 22:37:45 +0100

    Create pool

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('simple biolerplate');
+import './index.css';
+import Pool from '../assets/pool.png';
+
+const ctx = document.getElementById('game').getContext('2d');
+const img = new Image();
+img.onload = function() {
+  ctx.drawImage(img, 0, 0, 1000, 548);
+};
+img.src = Pool;
